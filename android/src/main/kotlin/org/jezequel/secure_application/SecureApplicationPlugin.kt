d9bcbc15580b9b04ded4bd34c7516e66740a7b29
JessicaShalomG <jessica@cuenca.com>
2021-06-30 18:49:06 -0500

    testing color

--- android/src/main/kotlin/org/jezequel/secure_application/SecureApplicationPlugin.kt
+++ android/src/main/kotlin/org/jezequel/secure_application/SecureApplicationPlugin.kt
@@ -81,7 +81,7 @@ public class SecureApplicationPlugin: FlutterPlugin, MethodCallHandler, Activity
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "secure") {
       activity?.window?.addFlags(LayoutParams.FLAG_SECURE)
-      activity?.window?.setBackgroundColor(Color.BLUE);
+      activity?.window?.setBackgroundColor(R.color.green);
       result.success(true)
     } else if (call.method == "open") {
       activity?.window?.clearFlags(LayoutParams.FLAG_SECURE)
