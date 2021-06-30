2c7f0e1e946ff667b8b6571ece2721f0cf6ad777
JessicaShalomG <jessica@cuenca.com>
2021-06-30 12:41:32 -0500

    renamed and cleaned

--- ios/Classes/SwiftSecureApplicationPlugin.swift
+++ ios/Classes/SwiftSecureApplicationPlugin.swift
@@ -31,49 +31,11 @@ public class SwiftSecureApplicationPlugin: NSObject, FlutterPlugin {
                 view.tag = 99699
 
                 var image : UIImage = UIImage(named:"splash")!
-                let bgImage = UIImageView(image: image)
-                bgImage.contentMode = .scaleToFill
-                bgImage.frame = view.bounds
+                let coverImage = UIImageView(image: image)
+                coverImage.contentMode = .scaleAspectFit
+                coverImage.frame = view.bounds
 
-                view.addSubview(bgImage)
-
-
-
-                /* let gradient: CAGradientLayer = CAGradientLayer()
-
-                 let to = UIColor(red: 0.07, green: 0.11, blue: 0.26, alpha: 1.00)
-                 let color = UIColor(red: 0.24, green: 0.29, blue: 0.46, alpha: 1.00)
-
-                 gradient.colors = [color.cgColor, to.cgColor]
-                 gradient.locations = [0.0 , 1.0]
-                 gradient.startPoint = CGPoint(x : 0.0, y : 0)
-                 gradient.endPoint = CGPoint(x :0.0, y: 0.90)
-                 gradient.frame = view.bounds
-
-                 view.layer.insertSublayer(gradient, at: 0)
-
-
-                 let yourLabel = UILabel(frame: CGRect(x: 80, y: 200, width: 300, height: 500))
-                   yourLabel.textColor = UIColor(red: 0.51, green: 0.56, blue: 0.73, alpha: 1.00)
-                   yourLabel.font = UIFont.init(name: "Roboto-Regular", size: 30.0)
-                   yourLabel.numberOfLines = 5
-                   //yourLabel.text = "CUENCA LA ÚNICA CUENTA QUE NECESITAS"
-
-                let paragraphStyle = NSMutableParagraphStyle()
-                let stringValue = "CUENCA\nLA ÚNICA CUENTA\nQUE NECESITAS"
-
-                let attributedString = NSMutableAttributedString(string: stringValue)
-
-                attributedString.addAttribute(NSAttributedString.Key.kern, value: 1.15, range: NSRange(location: 0, length: stringValue.count))
-
-                attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(red: 0.00, green: 0.71, blue: 0.68, alpha: 1.00), range: NSRange(location:9,length:6))
-
-                paragraphStyle.lineSpacing = 5 // Whatever line spacing you want in points
-                attributedString.addAttribute(.paragraphStyle, value:paragraphStyle, range: NSRange(location: 0, length: stringValue.count))
-                yourLabel.attributedText = attributedString;
-
-
-                view.addSubview(yourLabel)*/
+                view.addSubview(coverImage)
 
                 window.addSubview(view)
                 window.bringSubviewToFront(view)
