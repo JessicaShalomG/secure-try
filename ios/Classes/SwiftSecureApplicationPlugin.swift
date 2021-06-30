9c53da7e5e9f5781e0b5586c550fecd254a8461d
JessicaShalomG <jessica@cuenca.com>
2021-06-29 19:43:25 -0500

    testing

--- ios/Classes/SwiftSecureApplicationPlugin.swift
+++ ios/Classes/SwiftSecureApplicationPlugin.swift
@@ -56,9 +56,9 @@ public class SwiftSecureApplicationPlugin: NSObject, FlutterPlugin {
 
                 let attributedString = NSMutableAttributedString(string: stringValue)
 
-                attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont(name: "Roboto-Medium", size: 30.0), range: NSRange(location: 0, length: stringValue.count))
+                attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont(name: "Roboto-Regular", size: 30.0), range: NSRange(location: 0, length: stringValue.count))
 
-                //attributedString.addAttribute(NSAttributedString.Key.kern, value: 1.15, range: NSRange(location: 0, length: stringValue.count))
+                attributedString.addAttribute(NSAttributedString.Key.kern, value: 1.15, range: NSRange(location: 0, length: stringValue.count))
 
                 attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(red: 0.00, green: 0.71, blue: 0.68, alpha: 1.00), range: NSRange(location:9,length:6))
 
